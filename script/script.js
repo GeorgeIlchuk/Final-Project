c7e2cde3e6a1092f190e14450c27f64ca722b380
George <georgiy.ilchuk2009@gmail.com>
2024-10-13 19:15:00 +0300

    1

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+  $('.carousele').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true, // Відображення крапок для навігації
+    arrows: true, // Відображення стрілок
+  });
+});
+
